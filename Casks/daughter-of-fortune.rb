9a343c680fb09b735b534b18472c1acf442f3c04
MasterofDeath01 <sri.koustubh@gmail.com>
2025-12-21 21:42:54 +0000

    Update daughter-of-fortune.rb

--- Casks/daughter-of-fortune.rb
+++ Casks/daughter-of-fortune.rb
@@ -1,9 +1,9 @@
-cask "daugher-of-fortune" do
+cask "daughter-of-fortune" do
   version "5.0.0"
   sha256 "1f18cf9dca1664b6c17504d84b932afd25d1c57a0232fb93dfe8d2922bb3a296"
 
   url "https://github.com/MasterofDeath01/homebrew-apps/releases/download/v#{version}/Daughter-of-Fortune.ttf"
-  name "Altone Trial Bold"
+  name "Daughter of Fortune"
   desc "Custom font installed via Homebrew"
   homepage "https://github.com/MasterofDeath01/homebrew-apps"
 
