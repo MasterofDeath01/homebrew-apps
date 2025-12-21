642bd3e6c5fbe7a3244d0ea655a75e1d7749d3aa
MasterofDeath01 <sri.koustubh@gmail.com>
2025-12-21 14:43:31 +0000

    Update adobe-downloader.rb

--- Casks/adobe-downloader.rb
+++ Casks/adobe-downloader.rb
@@ -1,5 +1,5 @@
 cask "adobe-downloader" do
-  version "latest"
+  version "2.1.5"
   sha256 "11b15673a8568f58bf7e237203aea6c2b8277c353dc60a51b7282f5ec0c64e20"
 
   url "https://github.com/X1a0He/Adobe-Downloader/releases/download/v#{version}/Adobe.Downloader.dmg",
