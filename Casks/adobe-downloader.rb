6c4c5cd94de54ad2714ac644e187165229927e28
MasterofDeath01 <sri.koustubh@gmail.com>
2025-12-21 14:46:57 +0000

    adobe-downloader.rb

--- Casks/adobe-downloader.rb
+++ Casks/adobe-downloader.rb
@@ -2,11 +2,19 @@ cask "adobe-downloader" do
   version "2.1.5"
   sha256 "11b15673a8568f58bf7e237203aea6c2b8277c353dc60a51b7282f5ec0c64e20"
 
+  # GitHub release URL
   url "https://github.com/X1a0He/Adobe-Downloader/releases/download/v#{version}/Adobe.Downloader.dmg",
       verified: "github.com/X1a0He/Adobe-Downloader/"
   name "Adobe Downloader"
   desc "Downloader for Adobe apps"
   homepage "https://github.com/X1a0He/Adobe-Downloader"
 
+  # Livecheck to detect new versions automatically
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  # Install the app
   app "Adobe.Downloader.app"
 end
