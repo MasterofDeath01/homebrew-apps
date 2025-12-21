4c615289825ac9805f7ca8d69cf2fe8af13dd112
MasterofDeath01 <sri.koustubh@gmail.com>
2025-12-21 21:49:01 +0000

    Update luminance-smallcaps.rb

--- Casks/luminance-smallcaps.rb
+++ Casks/luminance-smallcaps.rb
@@ -2,8 +2,7 @@ cask "luminance-smallcaps" do
   version "10.0.0"
   sha256 "3983a4da69af2f9a8768a06d8e6d522925f2fdfd0bf6368fa08ee1fbb027fce1"
 
-  url "https://github.com/MasterofDeath01/homebrew-apps/releases/download/v#{version}/Luminance-OT-W00-Smallcaps.ttf
-"
+  url "https://github.com/MasterofDeath01/homebrew-apps/releases/download/v#{version}/Luminance-OT-W00-Smallcaps.ttf"
   name "Luminance Smallcaps Font"
   desc "Custom font installed via Homebrew"
   homepage "https://github.com/MasterofDeath01/homebrew-apps"
