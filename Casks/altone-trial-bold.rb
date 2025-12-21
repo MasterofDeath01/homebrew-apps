1d5f1c984eb20ccd4ab2bda67409fac67650d657
MasterofDeath01 <sri.koustubh@gmail.com>
2025-12-21 17:08:58 +0000

    Update altone-trial-bold.rb

--- Casks/altone-trial-bold.rb
+++ Casks/altone-trial-bold.rb
@@ -2,7 +2,7 @@ cask "altone-trial-bold" do
   version :latest
   sha256 :no_check
 
-  url "https://drive.usercontent.google.com/download?id=1vC_pDtU0xWuEd4dw8VzcO7Rjt7yureFB&export=download&authuser=0"
+  url "https://drive.usercontent.google.com/download?id=1vC_pDtU0xWuEd4dw8VzcO7Rjt7yureFB&export=download&authuser=0",
       verified: "drive.google.com"
   name "Altone Trial Bold"
   desc "Custom font installed via Homebrew"
