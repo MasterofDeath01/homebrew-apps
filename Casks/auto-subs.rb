04fe328234f0db8a2fe68eb3278272521dfa03c1
MasterofDeath01 <sri.koustubh@gmail.com>
2025-12-21 15:16:47 +0000

    Update auto-subs.rb

--- Casks/auto-subs.rb
+++ Casks/auto-subs.rb
@@ -2,8 +2,7 @@ cask "auto-subs" do
   version "3.0.8"
   sha256 "ed8f8d271427e0dd3ff7b27b8e63afaf524210adb3d5f87746b01a11028e9ddf"
 
-  url "https://github.com/tmoroney/auto-subs/releases/download/v#{version}/AutoSubs-Mac-ARM.pkg",
-      verified: "github.com/tmoroney/auto-subs/"
+  url "https://github.com/tmoroney/auto-subs/releases/download/v#{version}/AutoSubs-Mac-ARM.pkg"
   name "Auto-Subs"
   desc "AI-powered subtitle generator and editor"
   homepage "https://github.com/tmoroney/auto-subs"
