03a66d018d1a68075b261cc5a95886f8057d9538
MasterofDeath01 <sri.koustubh@gmail.com>
2025-12-21 21:47:42 +0000

    Update lemonmilk-regular.rb

--- Casks/lemonmilk-regular.rb
+++ Casks/lemonmilk-regular.rb
@@ -3,11 +3,9 @@ cask "lemonmilk-regular" do
   sha256 "de77a61b842958dc4efbefccb5532824f2d5efd738ff5fecc0d5d6eb6528896b"
 
   url "https://github.com/MasterofDeath01/homebrew-apps/releases/download/v#{version}/LEMONMILK-Regular.otf"
-"
   name "LEMONMILK Regular"
   desc "Custom font installed via Homebrew"
   homepage "https://github.com/MasterofDeath01/homebrew-apps"
 
-  font "LEMONMILK-Regular.otf
-"
+  font "LEMONMILK-Regular.otf"
 end
