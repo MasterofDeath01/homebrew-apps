3162a88e9cb5da13b1247419a0ef25c4d54cb7bb
MasterofDeath01 <sri.koustubh@gmail.com>
2025-12-22 18:46:02 +0000

    Update vencord-installer.rb

--- Casks/vencord-installer.rb
+++ Casks/vencord-installer.rb
@@ -2,7 +2,7 @@ cask "vencord-installer" do
   version "1.4.0"
   sha256 "b7b4d38643223df94f3af937ad05cd25c68d0b1aa5b6da52b71aee4593da8817"
 
-  url "https://github.com/Vencord/Installer/releases/v#{version}/download/VencordInstaller.MacOs.zip"
+  url "https://github.com/Vencord/Installer/releases/download/v#{version}/VencordInstaller.MacOs.zip"
   name "Vencord Installer"
   desc "Vencord for discord"
   homepage "https://github.com/MasterofDeath01/homebrew-apps"
