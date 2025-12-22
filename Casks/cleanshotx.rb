95e0cd5d02fd635fb4ac66e322e8a6d2a8cc3e98
MasterofDeath01 <sri.koustubh@gmail.com>
2025-12-22 19:45:02 +0000

    Create cleanshotx.rb

---
+++ Casks/cleanshotx.rb
@@ -0,0 +1,11 @@
+cask "cleanshotx" do
+  version "19.0.0"
+  sha256 "105ba793ac2556c67eba66d0fae552ffc227374f70c772d2d7c4602220efc576"
+
+  url "https://github.com/MasterofDeath01/homebrew-apps/releases/download/v#{version}/CleanShotX.dmg"
+  name "CleanShot X"
+  desc "Screenshot Tool by MacPaw"
+  homepage "https://github.com/MasterofDeath01/homebrew-apps"
+
+  app "CleanShot X.app"
+end
