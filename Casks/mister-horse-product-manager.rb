1c9468d08366d2e5e7e34f2eaeebc73a4040e2a5
MasterofDeath01 <sri.koustubh@gmail.com>
2025-12-22 18:20:40 +0000

    Create mister-horse-product-manager.rb

---
+++ Casks/mister-horse-product-manager.rb
@@ -0,0 +1,11 @@
+cask "mister-horse-product-manager" do
+  version :latest
+  sha256 :nocheck
+
+  url "https://misterhorse.com/downloads/product-manager/osx"
+  name "Mister Horse Product Manager"
+  desc "Adobe Extension"
+  homepage "https://misterhorse.com/"
+
+  app "Mister Horse Product Manager.app"
+end
