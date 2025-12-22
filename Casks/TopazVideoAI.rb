ad47baaa10a07ce2793ad89d041fd1a9986389ca
MasterofDeath01 <sri.koustubh@gmail.com>
2025-12-22 15:34:05 +0000

    Create TopazVideoAI.rb

---
+++ Casks/TopazVideoAI.rb
@@ -0,0 +1,11 @@
+cask "topaz-video-enhance-ai" do
+  version "18.0.0"
+  sha256 "4304e8ca31a92924a3a546fc825032e2bf279cd243cb0392cb7c2b9a4b18da52"
+
+  url "https://github.com/MasterofDeath01/homebrew-apps/releases/download/v#{version}/Topaz-Video-AI.dmg"
+  name "Topaz Video AI"
+  desc "Upscale Video Footage"
+  homepage "https://github.com/MasterofDeath01/homebrew-apps"
+
+  app "Topaz Video AI.app"
+end
