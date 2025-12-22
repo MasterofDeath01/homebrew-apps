e146e54c7cdf05da21099ac6f732b273e7760380
MasterofDeath01 <sri.koustubh@gmail.com>
2025-12-22 03:28:56 +0000

    Update the-amazing-spider-man.rb

--- Casks/the-amazing-spider-man.rb
+++ Casks/the-amazing-spider-man.rb
@@ -1,6 +1,6 @@
 cask "the-amazing-spider-man" do
   version "14.0.0"
-  sha256 "ee10f14ebf95b6a54ad60ec0ad08825af3f39114e400cadc978adbf3f5548905"
+  sha256 "ef25ad0dc0e2b42232357445f4370ab0e0eee4cf6152e1d6e51e6d703ddde"
 
   url "https://github.com/MasterofDeath01/homebrew-apps/releases/download/v#{version}/The-Amazing-Spider-Man.ttf"
   name "The Amazing Spiderman"
