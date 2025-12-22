cb2d11ed727949a87e3e392672f830097c38c9e4
MasterofDeath01 <sri.koustubh@gmail.com>
2025-12-22 15:29:53 +0000

    Create CleanMyMacX.rb

---
+++ Casks/CleanMyMacX.rb
@@ -0,0 +1,11 @@
+cask "cleanmymacx" do
+  version "17.0.0"
+  sha256 "ee32702611a6be94278235d53cd9293c266200a77930e1cd3ad3b30a2b52d04a"
+
+  url "https://github.com/MasterofDeath01/homebrew-apps/releases/download/v#{version}/CleanMyMacX.dmg"
+  name "CleanMyMacX"
+  desc "MacOS Cleaner Debloating Tool"
+  homepage "https://github.com/MasterofDeath01/homebrew-apps"
+
+  app "CleanMyMac X.app"
+end
