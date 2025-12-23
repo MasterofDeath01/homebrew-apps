aaa1f31a599af0b874578baf0c18f16f4d36439b
MasterofDeath01 <sri.koustubh@gmail.com>
2025-12-23 00:06:14 +0000

    Update zxp-installer.rb

--- Casks/zxp-installer.rb
+++ Casks/zxp-installer.rb
@@ -1,6 +1,6 @@
 cask "zxp-installer" do
-  version "latest"
-  sha256 "bf16160b3cb485b17ea9574bb2719c2643adcb873205eba9f14619ff695308d0"
+  version :latest
+  sha256 :no_check
 
   url "https://updates.aescripts.com/zxp-installer/mac/aescripts%20+%20aeplugins%20zxp%20installer%20(setup).dmg"
   name "ZXP Installer"
