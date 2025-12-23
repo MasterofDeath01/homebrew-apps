73be88245e32b1e075bb506cf3d89d209c09a667
MasterofDeath01 <sri.koustubh@gmail.com>
2025-12-23 14:53:50 +0000

    Create compacta.rb

---
+++ Casks/compacta.rb
@@ -0,0 +1,11 @@
+cask "compacta" do
+  version "20.0.0"
+  sha256 "b8730b1ac4ce5957fab4e89be2ba977fb75b094bde7b60b675777ce9c9be5cea"
+
+  url "https://github.com/MasterofDeath01/homebrew-apps/releases/download/v#{version}/compacta.ttf"
+  name "Compacta Font"
+  desc "Custom font installed via Homebrew"
+  homepage "https://github.com/MasterofDeath01/homebrew-apps"
+
+  font "compacta.ttf"
+end
