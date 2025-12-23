9d05bcfc11d0b8d0b376e464194804866633f320
MasterofDeath01 <sri.koustubh@gmail.com>
2025-12-23 00:02:57 +0000

    Create zxp-installer.rb

---
+++ Casks/zxp-installer.rb
@@ -0,0 +1,11 @@
+cask "zxp-installer" do
+  version "latest"
+  sha256 "bf16160b3cb485b17ea9574bb2719c2643adcb873205eba9f14619ff695308d0"
+
+  url "https://updates.aescripts.com/zxp-installer/mac/aescripts%20+%20aeplugins%20zxp%20installer%20(setup).dmg"
+  name "ZXP Installer"
+  desc "Installs ZXP Files for Adobe apps"
+  homepage "https://updates.aescripts.com/zxp-installer"
+
+  app "ZXP Installer.app"
+end
