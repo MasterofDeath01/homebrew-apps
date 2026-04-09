56d3ae96d14c4311206b62300f5f407cf59eeb71
MasterofDeath01 <sri.koustubh@gmail.com>
2026-04-09 21:19:37 +0100

    Update topaz-video-enhance-ai.rb

--- Casks/topaz-video-enhance-ai.rb
+++ Casks/topaz-video-enhance-ai.rb
@@ -1,6 +1,6 @@
 cask "topaz-video-enhance-ai" do
   version "18.0.0"
-  sha256 "4304e8ca31a92924a3a546fc825032e2bf279cd243cb0392cb7c2b9a4b18da52"
+  sha256 "080cc3d567ae530a8a131b056c55431ab23ed35ab27c1a01afce967e129876d4"
 
   url "https://github.com/MasterofDeath01/homebrew-apps/releases/download/v#{version}/Topaz-Video.dmg"
   name "Topaz Video AI"
