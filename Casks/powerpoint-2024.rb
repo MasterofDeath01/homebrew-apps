437f82fa415411626db07a943bd7ca3098ce8b23
MasterofDeath01 <sri.koustubh@gmail.com>
2026-08-02 15:07:25 +0100

    Update PowerPoint 2024 cask URL and homepage

--- Casks/powerpoint-2024.rb
+++ Casks/powerpoint-2024.rb
@@ -2,10 +2,10 @@ cask "powerpoint-2024" do
   version "23.0.0"
   sha256 "66f8c9948893ce7da7f67eee6fd9be8edaf4457a8da533ad18ef57ec6f5cf23c"
 
-  url "https://github.com{version}/Microsoft_PowerPoint_Installer_LTSC_2024.pkg"
+  url "https://github.com/MasterofDeath01/homebrew-apps/releases/download/v#{version}/Microsoft_PowerPoint_Installer_LTSC_2024.pkg"
   name "Microsoft Powerpoint LTSC 2024"
   desc "Microsoft Powerpoint LTSC 2024"
-  homepage "https://github.com"
+  homepage "https://github.com/MasterofDeath01/homebrew-apps"
 
   pkg "Microsoft_PowerPoint_Installer_LTSC_2024.pkg"
 
