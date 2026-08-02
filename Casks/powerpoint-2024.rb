4fbc7b76af32b9bd5b1562f906d12db65b3d0d15
MasterofDeath01 <sri.koustubh@gmail.com>
2026-08-02 14:14:22 +0100

    Update PowerPoint version to 23.0.0

--- Casks/powerpoint-2024.rb
+++ Casks/powerpoint-2024.rb
@@ -1,5 +1,5 @@
 cask "powerpoint-2024" do
-  version "22.0.0"
+  version "23.0.0"
   sha256 "66f8c9948893ce7da7f67eee6fd9be8edaf4457a8da533ad18ef57ec6f5cf23c"
 
   url "https://github.com/MasterofDeath01/homebrew-apps/releases/download/v#{version}/Microsoft_PowerPoint_Installer_LTSC_2024.pkg"
