88337dbdb5ebc4f78f9b1c2bcc0f685e72a46574
MasterofDeath01 <sri.koustubh@gmail.com>
2026-08-02 15:35:14 +0100

    Update Excel 2024 Cask version and URL

--- Casks/excel-2024.rb
+++ Casks/excel-2024.rb
@@ -1,11 +1,13 @@
 cask "excel-2024" do
-  version "24.0.0"
+  version "23.0.0"
   sha256 "284740a0bf64c744b49cabcf459bfc4a04de34326c8df154ce884e6ed4b4b223"
 
-  url "https://github.com/MasterofDeath01/homebrew-apps/releases/download/v#{version}/Microsoft_Excel_Installer_LTSC_2024.pkg"
+  url "https://github.com{version}/Microsoft_Excel_Installer_LTSC_2024.pkg"
   name "Microsoft Excel LTSC 2024"
-  desc "Excel LTSC 2024"
+  desc "Microsoft Excel LTSC 2024"
   homepage "https://github.com/MasterofDeath01/homebrew-apps"
+
+  depends_on macos: :big_sur
 
   pkg "Microsoft_Excel_Installer_LTSC_2024.pkg"
 
