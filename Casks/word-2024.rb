b84be3ea2c5679b7146708d67bcc69af145b82b9
MasterofDeath01 <sri.koustubh@gmail.com>
2026-08-02 14:14:07 +0100

    Create word-2024.rb

---
+++ Casks/word-2024.rb
@@ -0,0 +1,13 @@
+cask "word-2024" do
+  version "22.0.0"
+  sha256 "f574b52d7a472ea41cb39092ce709c6110f7e81f2b928f1df6661883f2ec40dc"
+
+  url "https://github.com/MasterofDeath01/homebrew-apps/releases/download/v#{version}/Microsoft_Word_Installer_LTSC_2024.pkg"
+  name "Microsoft Word LTSC 2024"
+  desc "Microsoft Word LTSC 2024"
+    homepage "https://github.com/MasterofDeath01/homebrew-apps"
+
+  pkg "Microsoft_Word_Installer_LTSC_2024.pkg"
+
+  uninstall pkgutil: "com.microsoft.package.Microsoft_Word.app"
+end
